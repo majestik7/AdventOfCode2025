e474809bf60d5662eba2f03c1111055a4cb32de1
majestik7 <richbram@hotmail.com>
2025-12-05 18:28:18 -0600

    .

--- Day5/Day5_b.py
+++ Day5/Day5_b.py
@@ -4,11 +4,10 @@ import copy
 
 current_script_dir = os.path.dirname(os.path.realpath(__file__))
 
-inputfile=current_script_dir + '\intest.txt'
+inputfile=current_script_dir + '\input.txt'
 
 answer = 0
 fresh = []
-ingredients = []
 all_fresh = []
 
 with open(inputfile, 'r') as file:
@@ -16,20 +15,56 @@ with open(inputfile, 'r') as file:
         l = line.strip()
         if l.count('-') > 0:
             l = l.split('-')
+            l = [int(item) for item in l]
             fresh.append(l)
-        #elif l == [] or l == '':
-            #continue
-        #else:
-            #ingredients.append(int(l))
 
-print(fresh)
-#print(ingredients)
+all_fresh = copy.deepcopy(fresh)
 
-for i in ingredients:
-    for f in fresh:
-        if int(f[0]) <= int(i) <= int(f[1]):
-            answer += 1
-            break
+changes = 1000
+x = 0
+es = 0
+while es < 5:
+    if changes < 1:
+        es += 1
+    #print('x',x)
+    changes = 0
+    while len(fresh) > 0:
+        #print(fresh)
+        f = fresh.pop(0)
+        #print('f',f)
+        for a in all_fresh:
+            if f != a:
+                if a[0] <= (f[0] - 1) <= a[1]:
+                    #print(a[0],'<=',f[0],'<',a[1])
+                    if f[1] > a[1]:
+                        a[1] = f[1]
+                        changes += 1
+                        break
+                if a[0] <= (f[1] + 1) <= a[1]:
+                    #print(a[0],'<=',f[1],'<',a[1])
+                    if f[0] < a[0]:
+                        a[0] = f[0]
+                        changes += 1
+                        break
+                if a[1] == f[1]:
+                    if f[0] < a[0]:
+                        a[0] = f[0]
+                        changes += 1
+                        break
+                        
+    print('changes',changes)
+    freshest =[]
+    for fr in all_fresh:
+        if fr not in freshest:
+            freshest.append(fr)
+    all_fresh = copy.deepcopy(freshest)
+    fresh = copy.deepcopy(freshest)
+    x += 1
 
+all_fresh.sort(key=lambda x: x[0])
+print(all_fresh)
+
+for n in all_fresh:
+    answer += n[1] - (n[0] - 1)
 
 print('\033[92m',answer,'\033[0m')
